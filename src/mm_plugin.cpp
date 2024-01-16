07e44797e2a2e0d52b599c7bb869b0034af2864c
Nikita Ushakov <ushakovn@tcd.ie>
2024-01-16 00:11:16 +0000

    Add some commands

--- src/mm_plugin.cpp
+++ src/mm_plugin.cpp
@@ -31,7 +31,52 @@ namespace cs2wizard {
 	PLUGIN_EXPOSE(WizardMMPlugin, g_Plugin);
 
 	CON_COMMAND_F(wizard, "Wizard control options", FCVAR_NONE) {
-		META_CONPRINTF("Sample command called by %d. Command: %s\n", context.GetPlayerSlot(), args.GetCommandString());
+		std::span arguments(args.ArgV(), args.ArgC());
+
+		if (args.ArgC() > 1) {
+			if (!strcmp(arguments[1], "version")) {
+				META_CONPRINTF(" Wizard %s\n", g_Plugin._context->GetVersion());
+				return;
+			} else if (!strcmp(arguments[1], "load")) {
+				return;
+			} else if (!strcmp(arguments[1], "unload")) {
+				return;
+			} else if (!strcmp(arguments[1], "modules")) {
+				return;
+			} else if (!strcmp(arguments[1], "plugins")) {
+				return;
+			} else if (!strcmp(arguments[1], "plugin")) {
+				return;
+			} else if (!strcmp(arguments[1], "module")) {
+				return;
+			}
+		}
+
+		META_CONPRINTF("Wizard Menu\n");
+		META_CONPRINTF("(c) untrustedmodders\n");
+		META_CONPRINTF("https://github.com/untrustedmodders\n");
+		META_CONPRINTF("usage: wizard <command> [arguments]\n");
+		META_CONPRINTF("  version       - Version information\n");
+		META_CONPRINTF("Plugin Manager commands:\n");
+		META_CONPRINTF("  load          - Load plugin manager\n");
+		META_CONPRINTF("  unload        - Unload plugin manager\n");
+		META_CONPRINTF("  modules       - List running modules\n");
+		META_CONPRINTF("  plugins       - List running plugins\n");
+		META_CONPRINTF("  plugin <name> - Show information about a module\n");
+		META_CONPRINTF("  module <name> - Show information about a plugin\n");
+		META_CONPRINTF("Package Manager commands:\n");
+		META_CONPRINTF("  -S   <name>   - Packages to install (space separated)\n");
+		META_CONPRINTF("  -Sf  <file>   - Packages to install (from file manifest)\n");
+		META_CONPRINTF("  -Sw  <link>   - Packages to install (from HTTP manifest)\n");
+		META_CONPRINTF("  -R   <name>   - Packages to uninstall (space separated)\n");
+		META_CONPRINTF("  -Ra           - Uninstall all packages\n");
+		META_CONPRINTF("  -U   <name>   - Packages to update (space separated)\n");
+		META_CONPRINTF("  -Ua           - Update all packages\n");
+		META_CONPRINTF("  -Q            - Print all local packages\n");
+		META_CONPRINTF("  -Qr           - Print all remote packages\n");
+		META_CONPRINTF("  -Qi  <name>   - Show information about local package\n");
+		META_CONPRINTF("  -Qri <name>   - Show information about remote package\n");
+		META_CONPRINTF("  -Qd           - Snapshot packages into manifest\n");
 	}
 
 	bool WizardMMPlugin::Load(PluginId id, ISmmAPI* ismm, char* error, size_t maxlen, bool late) {
@@ -58,9 +103,7 @@ namespace cs2wizard {
 	}
 
 	bool WizardMMPlugin::Unload(char* error, size_t maxlen) {
-
 		_context.reset();
-
 		return true;
 	}
 
@@ -80,7 +123,7 @@ namespace cs2wizard {
 	}
 
 	const char* WizardMMPlugin::GetVersion() {
-		return "1.0.0.0";
+		return CS2WIZARD_PROJECT_VERSION;
 	}
 
 	const char* WizardMMPlugin::GetDate() {
@@ -96,15 +139,15 @@ namespace cs2wizard {
 	}
 
 	const char* WizardMMPlugin::GetDescription() {
-		return "Package & Plugin Manager";
+		return CS2WIZARD_PROJECT_DESCRIPTION;
 	}
 
 	const char* WizardMMPlugin::GetName() {
-		return "CS2 Wizard";
+		return CS2WIZARD_PROJECT_NAME;
 	}
 
 	const char* WizardMMPlugin::GetURL() {
-		return "https://github.com/untrustedmodders/cs2wizard";
+		return CS2WIZARD_PROJECT_HOMEPAGE_URL;
 	}
 
 }
