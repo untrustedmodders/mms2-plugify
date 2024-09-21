278b6db9782977c459e2453f095b339b81505595
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-09-21 22:19:56 +0300

    Update CMake things

---
+++ cmake/platform/shared.cmake
@@ -0,0 +1,24 @@
+# cs2-plugify
+# Copyright (C) 2024 untrustedmodders
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+
+if(UNIX)
+	if(APPLE)
+		set(MACOS TRUE)
+	else()
+		set(LINUX TRUE)
+	endif()
+endif()
+
+if(WIN32)
+	if(NOT MSVC)
+		message(FATAL_ERROR "MSVC restricted")
+	endif()
+
+	set(WINDOWS TRUE)
+endif()
+
+set(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING
+	"Only do Release and Debug"
+	FORCE
+)
---
+++ cmake/plugify.cmake
@@ -0,0 +1,41 @@
+# cs2-plugify
+# Copyright (C) 2024 untrustedmodders
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+
+if(NOT PLUGIFY_DIR)
+	message(FATAL_ERROR "PLUGIFY_DIR is empty")
+endif()
+
+set(PLUGIFY_BINARY_DIR "plugify")
+
+# We target plugify as object to have exported methods of plugify
+set(PLUGIFY_BUILD_OBJECT_LIB ON CACHE INTERNAL "")
+
+if(LINUX)
+	set(PLUGIFY_USE_STATIC_STDLIB ON CACHE INTERNAL "")
+endif()
+
+
+set(PLUGIFY_COMPILE_DEFINITIONS
+	${PLUGIFY_COMPILE_DEFINITIONS}
+
+	PLUGIFY_PROJECT_YEAR="${PROJECT_BUILD_DATE_YEAR}"
+	PLUGIFY_PROJECT_NAME="${PROJECT_NAME}"
+	PLUGIFY_PROJECT_DESCRIPTION="${CMAKE_PROJECT_DESCRIPTION}"
+	PLUGIFY_PROJECT_HOMEPAGE_URL="${CMAKE_PROJECT_HOMEPAGE_URL}"
+	PLUGIFY_PROJECT_VERSION="${PLUGIFY_PROJECT_VERSION}"
+)
+
+set(PLUGIFY_LINK_LIBRARIES ${LOGGER_BINARY_DIR} miniz)
+
+if(WIN32)
+	list(APPEND PLUGIFY_LINK_LIBRARIES winhttp.lib)
+else()
+	list(APPEND PLUGIFY_LINK_LIBRARIES curl)
+endif()
+
+if(NOT COMPILER_SUPPORTS_FORMAT)
+	list(APPEND PLUGIFY_LINK_LIBRARIES fmt::fmt-header-only)
+endif()
+
+add_subdirectory(${PLUGIFY_DIR} ${PLUGIFY_BINARY_DIR})
